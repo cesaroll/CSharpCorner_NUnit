7f67ebfdf3dfc319dbbef08f66ac112bcd25ea96
Cesar Lopez <cesarl@outlook.com>
2016-03-01 12:49:59 -0700

    Initial

---
+++ IntroNUnitTest/MyNUnitTestClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace IntroNUnitTest
+{
+    [TestFixture]
+    public class MyNUnitTestClass
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            int x = 20, y = 20;
+            Assert.AreEqual(x,y);
+        }
+
+        [Test]
+        public void NagativeTest()
+        {
+            if(true)
+                Assert.Fail("This test is a failure");
+        }
+
+        [Test, ExpectedException(typeof (NotSupportedException))]
+        public void ExpectedExceptionTest()
+        {
+            throw new NotSupportedException();
+        }
+
+        [Test, Ignore]
+        public void NotImplementedException()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
